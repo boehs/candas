367620cbb63db4bee0582643a0bc7175cc119499
Evan Boehs <evan@boehs.org>
2022-11-14 14:07:17 -0500

    one more

--- src/routes/(main)/course/[id]/(announcements)/announcements.tsx
+++ src/routes/(main)/course/[id]/(announcements)/announcements.tsx
@@ -4,19 +4,23 @@ import { useAnnouncements } from "../(announcements)"
 
 export default function Announcements() {
     const announcements = useAnnouncements()
-    
+
     return <>
         <table>
-            <tr>
-                <th>Title</th>
-                <th>Date</th>
-            </tr>
-            <For each={announcements()}>
-                {announcement => <tr>
-                    <td><A href={`../announcement/${announcement.id}`}>{announcement.title}</A></td>
-                    <td>{(new Date(announcement.posted_at)).toLocaleDateString()}</td>
-                </tr>}
-            </For>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <For each={announcements()}>
+                    {announcement => <tr>
+                        <td><A href={`../announcement/${announcement.id}`}>{announcement.title}</A></td>
+                        <td>{(new Date(announcement.posted_at)).toLocaleDateString()}</td>
+                    </tr>}
+                </For>
+            </tbody>
         </table>
     </>
 }
