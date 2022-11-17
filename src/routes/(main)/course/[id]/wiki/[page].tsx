71dbcaa6e63c9aa51b2063f8cc6bb69ead2af97a
Evan Boehs <evan@boehs.org>
2022-11-17 12:46:40 -0500

    Breakout pages table to function for reuse

--- src/routes/(main)/course/[id]/wiki/[page].tsx
+++ src/routes/(main)/course/[id]/wiki/[page].tsx
@@ -1,12 +1,7 @@
-import { A, useNavigate } from "@solidjs/router"
-import { For, Show, Suspense } from "solid-js"
-import { ErrorBoundary, RouteDataArgs, Title, useRouteData } from "solid-start"
+import { RouteDataArgs, Title, useRouteData } from "solid-start"
 import { createServerData$ } from "solid-start/server"
-import NoContent from "~/components/noContent"
-import Table from "~/components/table"
-import Tr from "~/components/tr"
 import api from "~/lib/api"
-import wikiCss from './wiki.module.scss'
+import { AllPages } from "."
 
 export function routeData({ params }: RouteDataArgs) {
     const wiki = createServerData$(async ([id,page]) => await api(`courses/${id}/pages/${page}`), {
@@ -20,26 +15,9 @@ export function routeData({ params }: RouteDataArgs) {
 
 export default function Assignments() {
     const { wiki, allPages } = useRouteData<typeof routeData>()
-    const navigate = useNavigate()
     console.log(allPages())
     return <>
         <Title>Wiki: Main</Title>
-        <Suspense>
-            <Show when={wiki() && wiki().body} fallback={<NoContent/>}>
-                <div class={wikiCss.wiki} innerHTML={wiki() && wiki().body} />
-            </Show>
-        </Suspense>
-        <ErrorBoundary>
-            <Show when={allPages()} fallback={<NoContent/>}>
-                <h2>Pages</h2>
-                <Table headers={['Title']}>
-                    <For each={allPages()}>
-                        {page => <Tr goal={() => navigate(`../${page.page_id}`)}>
-                            <td><A href={`../${page.page_id}`}>{page.title}</A></td>
-                        </Tr>}
-                    </For>
-                </Table>
-            </Show>
-        </ErrorBoundary>
+        <AllPages wiki={wiki} allPages={allPages} prefix='../'/>
     </>
 }
--- src/routes/(main)/course/[id]/wiki/index.tsx
+++ src/routes/(main)/course/[id]/wiki/index.tsx
@@ -1,7 +1,10 @@
-import { Show, Suspense } from "solid-js"
-import { RouteDataArgs, Title, useRouteData } from "solid-start"
+import { useNavigate } from "@solidjs/router"
+import { For, Resource, Show, Suspense } from "solid-js"
+import ErrorBoundary, { A, RouteDataArgs, Title, useRouteData } from "solid-start"
 import { createServerData$ } from "solid-start/server"
 import NoContent from "~/components/noContent"
+import Table from "~/components/table"
+import Tr from "~/components/tr"
 import api from "~/lib/api"
 import wikiCss from './wiki.module.scss'
 
@@ -9,17 +12,43 @@ export function routeData({ params }: RouteDataArgs) {
     const wiki = createServerData$(async ([id]) => await api(`courses/${id}/front_page`), {
         key: () => [params.id]
     })
-    return { wiki }
+    const allPages = createServerData$(async ([id,page]) => await api(`courses/${id}/pages/`), {
+        key: () => [params.id,params.page]
+    })
+    return { wiki, allPages }
 }
 
 export default function Assignments() {
-    const { wiki } = useRouteData<typeof routeData>()
+    const { wiki, allPages } = useRouteData<typeof routeData>()
     return <>
         <Title>Wiki: Main</Title>
+        <AllPages wiki={wiki} allPages={allPages} prefix=''/>
+    </>
+}
+
+export function AllPages(props: {
+    wiki: Resource<any>,
+    allPages: Resource<any>
+    prefix: string
+}) {
+    const navigate = useNavigate()
+    return <>
         <Suspense>
-            <Show when={wiki() && wiki().body} fallback={<NoContent/>}>
-                <div class={wikiCss.wiki} innerHTML={wiki() && wiki().body} />
+            <Show when={props.wiki() && props.wiki().body} fallback={<NoContent />}>
+                <div class={wikiCss.wiki} innerHTML={props.wiki() && props.wiki().body} />
             </Show>
         </Suspense>
+        <ErrorBoundary>
+            <Show when={props.allPages()}>
+                <h2>Pages</h2>
+                <Table headers={['Title']}>
+                    <For each={props.allPages()}>
+                        {page => <Tr goal={() => navigate(`${props.prefix}${page.page_id}`)}>
+                            <td><A href={`${props.prefix}${page.page_id}`}>{page.title}</A></td>
+                        </Tr>}
+                    </For>
+                </Table>
+            </Show>
+        </ErrorBoundary>
     </>
 }
